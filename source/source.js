3700e883288de24506d83d50b0dd9b7c62ae5833
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-03-04 22:09:34 -0300

    Conserto da atualização da temperatura

--- source/source.js
+++ source/source.js
@@ -1,5 +1,3 @@
-import Main from "./components/Main.js";
-import requestTemperature from './adapters/adapters.js';
 import {getLocation, getLocation2} from './services/services.js';
 import position from './context/Position/Position.js';
 
