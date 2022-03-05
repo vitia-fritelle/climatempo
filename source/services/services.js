d5df7c285d1f15747501d216a9eadb62ca288b4f
Vitor Junior <71563179+vitia-fritelle@users.noreply.github.com>
2022-03-04 23:31:23 -0300

    Refatoração

--- source/services/services.js
+++ source/services/services.js
@@ -3,28 +3,27 @@ import position from "../context/Position/Position.js";
 import Page from "../pages/Page.js";
 import temperature from "../context/Temperature/Temperature.js";
 
-export const getLocation = () => {
+export const onFirstMount = () => {
     
     if('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(
             ({coords}) => {
                 position.setLatitude(coords.latitude);
                 position.setLongitude(coords.longitude);
-                requestTemperature().then(({data}) => {
-                    temperature.setTemperature(data.main.temp)
-                    Page();
-                });
+                requestTemperature().then(renderPage);
             }, (error) => console.log(error)
         )
     } else {
         console.log('Usuário não autorizou')
     }  
+    return null;
 }
 
-export const getLocation2 = () => {
-    requestTemperature().then(({data}) => {
-        temperature.setTemperature(data.main.temp);
-        Page();
-    })
+export const getLocation = () => requestTemperature().then(renderPage);
+
+const renderPage = ({data}) => {
+    
+    temperature.setTemperature(data.main.temp);
+    Page();
+    return null;
 }
- 
